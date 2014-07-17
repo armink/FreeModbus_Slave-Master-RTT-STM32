c3325ad15ee18f75b8fa4427f5b02eed77dc379c
armink <armink.ztl@gmail.com>
2014-07-17 16:42:45 +0800

    1、【优化】主机从机串口接收数据逻辑，在T3.5到了之后，迅速关闭串口接收，避免在极端条件下可能导致接收数据区数据被覆盖的问题。感谢网友@zhangshixing 的反馈。
    Signed-off-by: armink <armink.ztl@gmail.com>

--- FreeModbus/modbus/rtu/mbrtu_m.c
+++ FreeModbus/modbus/rtu/mbrtu_m.c
@@ -356,9 +356,10 @@ xMBMasterRTUTimerExpired(void)
 		xNeedPoll = xMBMasterPortEventPost(EV_MASTER_READY);
 		break;
 
-		/* A frame was received and t35 expired. Notify the listener that
+		/* A frame was received and t35 expired. Close serial receive and notify the listener that
 		 * a new frame was received. */
 	case STATE_M_RX_RCV:
+		vMBMasterPortSerialEnable( FALSE, FALSE );
 		xNeedPoll = xMBMasterPortEventPost(EV_MASTER_FRAME_RECEIVED);
 		break;
 
